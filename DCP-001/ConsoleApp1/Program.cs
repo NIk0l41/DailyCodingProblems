3b575ff5d34b6d357b9b0f058b79fc5a7bc0cac6
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-01-11 11:14:12 +1000

    DCP-001

---
+++ DCP-001/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] n = { 10,15,3,7 };
+            int k =17;
+            Console.WriteLine(Check1(n, k));
+            Console.ReadLine();
+        }
+
+        static bool Check1(int[] n, int k) {
+            bool o = false;
+            for (int i = 0; i < n.Length; i++) {
+                for (int j = i + 1; j < n.Length; j++) {
+                    if (n[i] + n[j] == k) {
+                        o = true;
+                        break;
+                    }
+                }
+            }
+            return o;
+        }
+    }
+}
