d3be9254999ca31a36852c3e589e41e980c49339
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-01-13 12:33:11 +1000

    DCP-003

---
+++ DCP-003/DCP-003/BinaryTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NIk0l41
+{
+    namespace BinaryTreeSorter {
+
+        class Node{
+            public string val;
+            public Node left;
+            public Node right;
+
+            public Node(string val, Node left = null, Node right = null){
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+
+            public void NewBranch(string side, Node left = null, Node right = null){
+
+            }
+
+        }
+    }
+}
---
+++ DCP-003/DCP-003/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DCP_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node node = new Node("Root", new Node("left", new Node("left.left")), new Node("right"));
+            if (deserialise(serialise(node)).left.left.val == "left.left") {
+                Console.WriteLine("Yay!");
+                Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Returns a string of selected branches.
+        /// </summary>
+        /// <param name="Root">The Root Node.</param>
+        /// <returns></returns>
+        static string serialise(Node Root) {
+            Node b = Root.left;
+            Node c = b.left;
+            return c.val;
+        }
+
+        static Node deserialise(string Tree) {
+            string[] parts = Tree.Split('.');
+            Node newTree = new Node("Root");
+            switch (parts[0]) {
+                case "left":
+                    newTree.left = new Node("left");
+                    switch (parts[1]) {
+                        case "left":
+                            newTree.left.left = new Node("left.left");
+                            break;
+                    }
+                    break;
+                case "right":
+                    newTree.right = new Node("right");
+                    break;
+            }
+            return newTree;
+        }
+    }
+
+    class Node{
+
+        public string val;
+        public Node left;
+        public Node right;
+
+        public Node(string val, Node left = null, Node right = null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public void NewBranch(string side, Node left = null, Node right = null) {
+            
+        }
+
+    }
+}
