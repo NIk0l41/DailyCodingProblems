de89abd6c4b18384021fd449a802e529a74e21b7
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-01-15 10:58:33 +1000

    DCP-005
    Init Commit

---
+++ DCP-005/DCP-005/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DCP_005{
+
+    internal class Program{
+
+        private static void Main(string[] args){
+            cons pair = new cons(3, 4);
+            Console.WriteLine(pair.a + ", " + pair.b);
+            Console.WriteLine("---");
+            Console.WriteLine(car(pair));
+            Console.WriteLine(cdr(pair));
+            Console.ReadLine();
+        }
+
+        private static int car(cons c){
+            return c.a;
+        }
+
+        private static int cdr(cons c){
+            return c.b;
+        }
+    }
+
+    internal class cons{
+        public int a;
+        public int b;
+
+        public cons(int a, int b){
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
