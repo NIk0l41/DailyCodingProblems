39bc6c7f54aff1801039df265f0570dec645d83c
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-01-21 10:55:46 +1000

    DCP-011

---
+++ DCP-011/DCP-011/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DCP_011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pre = "s";
+            string[] coll = {"snake", "spiral", "leprechaun", "spider" };
+            return0(pre, coll);
+            Console.ReadLine();
+        }
+
+        static void return0(string prefix, string[] collection) {
+            string[] output = new string[collection.Length];
+            for (int i = 0; i < collection.Length; i++) {
+                if (collection[i].StartsWith(prefix)) {
+                    Console.WriteLine(collection[i]);
+                }
+            }
+        }
+    }
+}
