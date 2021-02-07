e8a65cf7706dc4a4f4d0cf47521c4ccfd4ad6e72
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-02-07 11:28:47 +1000

    DCP-016 (Unfinished)
    What the fuck?

---
+++ DCP-016/DCP-016/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace DCP_016
+{
+    class Program {
+
+
+        public static int[] log = new int[256];
+
+        static void Main(string[] args) {
+
+            
+
+        }
+
+        static void record(int id) {
+
+        }
+
+        static int get_last(int i) {
+            
+        }
+    }
+
+}
